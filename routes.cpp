37cb49d54e7669a644456d76b0558146803d6379
Mahesh <mkm63806@gmail.com>
2023-08-22 23:02:49 +0530

    Third commit

--- routes.cpp
+++ routes.cpp
@@ -8,11 +8,11 @@ crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int Add = input["first"].i() + input["second"].i();
-    return crow::response{"/{result/:/" + std::to_string(Add) + "/ }"};
+    return crow::response{"/{result /:/" + std::to_string(Add) + "/ }"};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int Sub = input["first"].i() - input["second"].i();
-    return crow::response{"/{result/:/" + std::to_string(Sub) + "/ }"};
+    return crow::response{"/{result /:/" + std::to_string(Sub) + "/ }"};
 }
